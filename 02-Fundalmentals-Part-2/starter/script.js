e8d1e6e87fdbef1a46b4b188af811ffdfd3918e1
PimentaA <trz.andre@gmail.com>
2022-10-23 17:34:41 +0100

    Add Reviewing Functions

--- 02-Fundalmentals-Part-2/starter/script.js
+++ 02-Fundalmentals-Part-2/starter/script.js
@@ -97,7 +97,7 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 console.log(yearsUntilRetirement(1991, 'André'));
 console.log(yearsUntilRetirement(1980, 'Bob'));
-*/
+
 
 //Functions calling other functions
 
@@ -113,3 +113,26 @@ function fruitProcessor(apples, oranges) {
   return juice;
 }
 console.log(fruitProcessor(2, 3));
+*/
+
+//Reviewing Functions
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years.`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired!`);
+    return -1;
+  }
+};
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
