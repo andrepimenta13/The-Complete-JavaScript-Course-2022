735eda81473a1281a197055e1f4cdb061870c35f
PimentaA <trz.andre@gmail.com>
2022-11-13 11:47:34 +0000

    Add ArraysMethods studies

--- 02-Fundalmentals-Part-2/starter/Assignments/ArraysMethods.js
+++ 02-Fundalmentals-Part-2/starter/Assignments/ArraysMethods.js
@@ -1,3 +1,39 @@
+//Arrays Methods
+/*
+const friends = ['Michael', 'Steven', 'Peter'];
+
+//push - Add elements to the end of the array and return the length
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+//unshift - Add elements to the beginning of the array and return the length
+friends.unshift('John');
+console.log(friends);
+
+//pop - Remove last element from the array and return the removed element
+friends.pop();
+console.log(friends);
+
+//shift - Remove first element from the array and return the removed element
+friends.shift();
+console.log(friends);
+
+//indexOf - Return the index of the certain element in the array
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob')); //Doesn't exist, so return -1
+
+//includes - simply return true if the element is present in the array and false if it's not
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+friends.push(23);
+console.log(friends.includes('23')); //returns false because it's testing with strict equality which means that it does not do type coercion.
+
+if (friends.includes('Steven')) {
+  console.log('You have a friend called Steven');
+}
+*/
+
 //Assignment
 
 const neighbours = ['Portugal', 'Sweden', 'Norway', 'Finland', 'Italy'];
