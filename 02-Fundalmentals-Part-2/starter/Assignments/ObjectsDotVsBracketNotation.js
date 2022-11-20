410a64fd72fd253c86b6917270499ac4e6191393
PimentaA <trz.andre@gmail.com>
2022-11-20 22:19:06 +0000

    Fix some things about Objects studies

--- 02-Fundalmentals-Part-2/starter/Assignments/ObjectsDotVsBracketNotation.js
+++ 02-Fundalmentals-Part-2/starter/Assignments/ObjectsDotVsBracketNotation.js
@@ -15,3 +15,11 @@ and a capital called ${myCountry.capital}.`);
 
 console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries
 and a capital called ${myCountry.capital}.`);
+
+//Increase the country's population by two million using dot notation, and then decrease it by two million using brackets notation.
+
+myCountry.population += 2;
+console.log(myCountry.population);
+
+myCountry['population'] -= 2;
+console.log(myCountry.population);
