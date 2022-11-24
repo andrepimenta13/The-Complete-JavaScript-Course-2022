e509376423252590a6816dd29833f00d19c60bec
PimentaA <trz.andre@gmail.com>
2022-11-24 10:36:17 +0000

    Add Object Methods

--- 02-Fundalmentals-Part-2/starter/script.js
+++ 02-Fundalmentals-Part-2/starter/script.js
@@ -247,7 +247,7 @@ const jonas = {
 
 console.log(Object.keys(jonas));
 console.log(Object.values(jonas));
-*/
+
 
 //Objects Dot vs. Bracket Notation
 
@@ -302,3 +302,70 @@ console.log(jonas);
 console.log(
   `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}.`
 );
+*/
+
+//Object Methods
+
+//Any function that is attached to an object is called a method.
+
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  birthYear: 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven'],
+  hasDriversLicense: false,
+
+  // calcAge: function (birthYear) {
+  //   return 2037 - birthYear;
+  // },
+
+  //with this method keyword
+  // calcAge: function () {
+  //   // console.log(this);
+  //   return 2037 - this.birthYear;
+  // },
+
+  //create a new property on the Jonas object
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${
+      this.job
+    }, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's licence.`;
+  },
+};
+
+console.log(jonas.calcAge());
+
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+console.log(jonas.getSummary());
+
+console.log(jonas);
+
+// console.log(jonas.calcAge(1991)); //access the function using dot notation
+// console.log(jonas['calcAge'](1991)); //access the function using brackets notation
+
+//Challenge
+// "Jonas is a 46-year old teacher, and he has a/no driver's licence"
+
+//Solution 1
+
+// getSummary: function () {
+//   this.summary = this.hasDriversLicense
+//     ? `${jonas.firstName} is a ${jonas.age}-year old ${jonas.job}, and he has a driver's licence.`
+//     : `${jonas.firstName} is a ${jonas.age}-year old ${jonas.job}, and he has no driver's licence.`;
+// }
+
+//Solution 2
+// getSummary: function () {
+//   return `${this.firstName} is a ${this.calcAge()}-year old ${
+//     this.job
+//   }, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's licence.`;
+// },
