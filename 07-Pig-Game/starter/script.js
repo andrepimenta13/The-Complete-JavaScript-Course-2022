f6af580bc5874013a7ea3118f8088c1845895f7b
PimentaA <trz.andre@gmail.com>
2023-02-20 20:36:03 +0000

    Starting the Pig Game Project and create first functionalities

---
+++ 07-Pig-Game/starter/script.js
@@ -0,0 +1,81 @@
+'use strict';
+
+// Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const currentScore0El = document.getElementById('current--0');
+const currentScore1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', diceRoll);
+
+function diceRoll() {
+  //1. Generate random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  //2. Display dice roll
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //3. Check for rolled 1
+
+  if (dice !== 1) {
+    //Add dice to current score
+    currentScore += dice;
+
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+}
+
+//Holds score
+// -Add current score to the total score
+// -If score >= 100 current player wins; If not, switch player
+
+btnHold.addEventListener('click', holdGame);
+
+function holdGame() {
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+}
+
+//User resets game
+// -Set all scores to 0
+// -Set player 1 as starting player
+
+btnNew.addEventListener('click', resetGame);
+
+function resetGame() {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  currentScore0El.textContent = 0;
+  currentScore1El.textContent = 0;
+  currentScore = 0;
+  activePlayer = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+}
