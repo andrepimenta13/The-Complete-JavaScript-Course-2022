2f5b1d95deb768f728a168b915d5d539385fd97c
PimentaA <trz.andre@gmail.com>
2023-03-09 12:32:08 +0000

    Add code for missing Coding Challenge1 and starting the Behind the Scenes section

---
+++ 05-Guess-My-Number/starter/CodingChallenge1.js
@@ -0,0 +1,15 @@
+//Playing the Game Again" functionality
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
