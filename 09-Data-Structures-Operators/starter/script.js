aa92c48bf2ca9d4bad195d60d3eb6db8e5b6e6fb
PimentaA <trz.andre@gmail.com>
2023-05-08 15:39:45 +0100

    Add Logical Assignment Operators

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -54,6 +54,48 @@ const restaurant = {
   },
 };
 
+////////////////////////////////////////////////////////////////
+// Logical Assignment Operators
+
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// OR assignment operator - this operator assigns a variable to a variable if that variable is currently falsy.
+
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests = rest1.numGuests || 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// console.log(rest1); // numGuests = 20
+// console.log(rest2); // numGuests = 10
+
+// nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+console.log(rest1); // numGuests = 0
+console.log(rest2); // numGuests = 10
+
+// AND assignment operator - when the first value is falsy, then immediately returns that falsy value
+
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
 /*
 ////////////////////////////////////////////////////////////////
 // Nullish Coalescing Operator (??)
