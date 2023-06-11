5287cccbf8d75d7544b210de475270b6b3e4ffda
PimentaA <trz.andre@gmail.com>
2023-06-11 16:00:11 +0100

    Add maps iteration studies

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -54,7 +54,46 @@ const restaurant = {
   },
 };
 
+/*
 ////////////////////////////////////////////////////////////////////
+// Maps: Iteration
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'Javascript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Iteration
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your answer?'));
+const answer = 3;
+console.log(answer);
+console.log(question.get(answer === question.get('correct')));
+
+// Convert map to an Array
+console.log([...question]);
+// console.log([...question.entries()]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+////////////////////////////////////////////////////////////////////
+// Maps: Fundamentals
 // Maps - A map is a data structure that we can use to map values to keys
 
 const rest = new Map();
@@ -97,7 +136,7 @@ console.log(rest);
 
 console.log(rest.get(arr));
 
-/*
+
 ////////////////////////////////////////////////////////////////////
 // Sets - are basically just a collection of unique values. A set can never have any duplicates.
 
