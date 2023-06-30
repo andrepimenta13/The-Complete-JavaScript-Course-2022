e6ca0db3626127e27c132619a1a13c18a1b704a6
PimentaA <trz.andre@gmail.com>
2023-06-30 16:24:18 +0100

    Add How Passwing Arguments studies

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 ///////////////////////////////////////////////////////////////
 // Default Parameters
 
@@ -29,3 +30,47 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
+
+///////////////////////////////////////////////////////////////
+// How Passing Arguments Works: Value vs Reference
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: '31239283127',
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === '31239283127') {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+// Javascript doesn't have passing by reference, only passing by value.
+
+*/
+
+///////////////////////////////////////////////////////////////
+// First-Class and Higher Order Functions
